a43187f15c212840cd21c47fc2e98f8903df1025
hamza5839 <53337796+hamza5839@users.noreply.github.com>
2026-04-25 00:47:00 +0500

    Add build and deploy functions in script.groovy

---
+++ script.groovy
@@ -0,0 +1,17 @@
+def buildJar() {
+    echo 'Building the jar file...'
+                sh 'mvn package' // Add your build commands here
+}
+def buildImage() {
+    echo 'Building the Docker image...'
+                withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'   
+                }
+                sh 'docker build -t devhamzaops/my-app:latest .' // Add your build commands here
+                sh 'docker push devhamzaops/my-app:latest' // Add your push commands,
+}
+
+def deployApp() {
+    echo 'Deploying...'
+                // Add your deploy commands here
+}
